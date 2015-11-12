daea971bf84f4345ca554cc86c3f9c0160bbab06
erkweb93 <erkweb@hotmail.com>
2015-11-12 00:53:15 -0200

    alterações

--- config.rb
+++ config.rb
@@ -3,12 +3,12 @@ require "breakpoint"
 
 project_type = :stand_alone
 http_path = "/"
-sass_dir = "assets/scss"
+sass_dir = "dir/scss"
 css_dir = "app/assets/css"
-images_dir ="assets/img"
-font_dir = "assets/fonts"
-javascript_dir = "assets/js"
+images_dir ="app/assets/images"
+font_dir = "app/assets/fonts"
+javascript_dir = "app/assets/js"
 line_comments = false
 preferred_syntax = :scss
-output_style = :compressed
+output_style = :expanded
 relative_assets = true
